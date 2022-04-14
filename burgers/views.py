729413b6129934ce489e21364b71fa36e2d42d76
tjdanielson <tessajdanielson@gmail.com>
2022-04-14 17:02:29 -0500

    Added a class view/get request for getting all burgers.

--- burgers/views.py
+++ burgers/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.decorators import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+from burgers.serializers import BurgerSerializer
+from .models import Burgers
 
 # Create your views here.
+
+class BurgerList(APIView, AllowAny):
+
+    def get(self, request):
+        burgers = Burgers.objects.all()
+        serializer = BurgerSerializer(burgers, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
